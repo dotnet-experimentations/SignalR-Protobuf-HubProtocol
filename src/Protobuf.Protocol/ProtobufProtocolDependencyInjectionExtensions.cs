30793190e0b4601949a9fbb9992eea003d769198
azalax <contact@ludovic-alarcon.com>
2019-08-04 19:45:17 +0200

    Dependency injection extension with AddProtobufProtocol

---
+++ src/Protobuf.Protocol/ProtobufProtocolDependencyInjectionExtensions.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using Microsoft.AspNetCore.SignalR.Protocol;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.Extensions.Logging;
+using Protobuf.Protocol;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class ProtobufProtocolDependencyInjectionExtensions
+    {
+        public static TBuilder AddProtobufProtocol<TBuilder>(this TBuilder builder) where TBuilder : ISignalRBuilder
+            => AddProtobufProtocol(builder, Enumerable.Empty<Type>());
+
+        public static TBuilder AddProtobufProtocol<TBuilder>(this TBuilder builder, IEnumerable<Type> protobufTypes) where TBuilder : ISignalRBuilder
+        {
+            var logger = builder.Services.BuildServiceProvider().GetRequiredService<ILogger<ProtobufHubProtocol>>();
+            builder.Services.TryAddEnumerable(ServiceDescriptor.Singleton<IHubProtocol>(new ProtobufHubProtocol(protobufTypes, logger)));
+            return builder;
+        }
+    }
+}
