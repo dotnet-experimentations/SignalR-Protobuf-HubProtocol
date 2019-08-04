dd19bf7a8b2d8fbba2874d8580e8498d3c56784e
tebeco <tebeco@gmail.com>
2019-08-04 16:05:03 +0200

    Add BenchmarkDotNet project

---
+++ perf/Protobuf.Protocol.Microbenchmarks/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Protobuf.Protocol.Microbenchmarks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
