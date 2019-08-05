83b4d4fc5aae5ef2a87e67d4f1717d7b5b5ab7bd
azalax <contact@ludovic-alarcon.com>
2019-08-05 20:55:53 +0200

    move argument serialization in a separate class

---
+++ src/Protobuf.Protocol/ArgumentSerializer.cs
@@ -0,0 +1,106 @@
+﻿using Google.Protobuf;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Protobuf.Protocol
+{
+    internal class ArgumentSerializer
+    {
+        private readonly Dictionary<Type, int> _protobufTypeToIndex;
+        private readonly Dictionary<int, Type> _indexToProtobufType;
+
+        private readonly int _numberOfNoProtobufObjectHandle = 4;
+
+        internal ArgumentSerializer(IEnumerable<Type> protobufTypes)
+        {
+            var size = protobufTypes.Count();
+            _protobufTypeToIndex = new Dictionary<Type, int>(size);
+            _indexToProtobufType = new Dictionary<int, Type>(size);
+
+            var enumerator = protobufTypes.GetEnumerator();
+
+            while (enumerator.MoveNext())
+            {
+                var index = _protobufTypeToIndex.Count + _numberOfNoProtobufObjectHandle + 1;
+                _protobufTypeToIndex[enumerator.Current] = index;
+                _indexToProtobufType[index] = enumerator.Current;
+            }
+        }
+
+        internal List<ArgumentDescriptor> SerializeArguments(object[] arguments)
+        {
+            var argumentsDescriptors = new List<ArgumentDescriptor>(arguments.Length);
+
+            for (var i = 0; i < arguments.Length; i++)
+            {
+                var argumentDescriptor = DescribeArgument(arguments[i]);
+                argumentsDescriptors.Add(argumentDescriptor);
+            }
+
+            return argumentsDescriptors;
+        }
+
+        internal ArgumentDescriptor SerializeArgument(object argument)
+        {
+            return DescribeArgument(argument);
+        }
+
+        private ArgumentDescriptor DescribeArgument(object argument)
+        {
+            switch (argument)
+            {
+                case string item:
+                    return new ArgumentDescriptor(2, Encoding.UTF8.GetBytes(item));
+                case int item:
+                    return new ArgumentDescriptor(3, BitConverter.GetBytes(item));
+                case double item:
+                    return new ArgumentDescriptor(4, BitConverter.GetBytes(item));
+                case IMessage item:
+                    return new ArgumentDescriptor(_protobufTypeToIndex[item.GetType()], item.ToByteArray());
+                default:
+                    return null;
+            }
+        }
+
+        internal object[] DeserializeArguments(List<ArgumentDescriptor> argumentsDescriptor)
+        {
+            var arguments = new List<object>();
+
+            for (var i = 0; i < argumentsDescriptor.Count; i++)
+            {
+                var currentDescriptor = argumentsDescriptor[i];
+
+                if (currentDescriptor.Type <= _numberOfNoProtobufObjectHandle)
+                {
+                    var argument = DeserializeNotProtobufObjectArgument(argumentsDescriptor[i]);
+                    arguments.Add(argument);
+                }
+                else
+                {
+                    var argument = (IMessage)Activator.CreateInstance(_indexToProtobufType[currentDescriptor.Type]);
+                    argument.MergeFrom(currentDescriptor.Argument);
+                    arguments.Add(argument);
+                }
+            }
+            return arguments.ToArray();
+        }
+
+        private object DeserializeNotProtobufObjectArgument(ArgumentDescriptor argumentDescriptor)
+        {
+            switch (argumentDescriptor.Type)
+            {
+                case 2:
+                    return Encoding.UTF8.GetString(argumentDescriptor.Argument);
+                case 3:
+                    return BitConverter.ToInt32(argumentDescriptor.Argument, 0);
+                case 4:
+                    return BitConverter.ToDouble(argumentDescriptor.Argument, 0);
+                default:
+                    return null;
+            }
+        }
+
+    }
+}
